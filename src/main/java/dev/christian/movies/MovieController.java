fa332f78f5d1862502e7e0af5d9cac8c00411bf5
ChristianHong <christian.hong.0902@gmail.com>
2024-05-10 11:21:43 -0500

    MovieService

--- src/main/java/dev/christian/movies/MovieController.java
+++ src/main/java/dev/christian/movies/MovieController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
@@ -15,9 +17,8 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
 }
