042a303ad5d7d61a632ccaabc6f12954e0c683af
ChristianHong <christian.hong.0902@gmail.com>
2024-05-11 13:22:23 -0500

    MovieService

--- src/main/java/dev/christian/movies/MovieController.java
+++ src/main/java/dev/christian/movies/MovieController.java
@@ -1,10 +1,12 @@
 package dev.christian.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,8 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id) {
+        return
+    }
 }
