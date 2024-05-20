78c0e4fbf459b38844d6577e2041eec2e2f09e29
ChristianHong <christian.hong.0902@gmail.com>
2024-05-19 19:48:46 -0500

    adding update for review

--- src/main/java/dev/christian/movies/ReviewController.java
+++ src/main/java/dev/christian/movies/ReviewController.java
@@ -18,5 +18,6 @@ public class ReviewController {
 
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
 }
